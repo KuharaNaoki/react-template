3e56e16b4526afbaf74e5076a738f052f2235830
KuharaNaoki <naoki.kuahra0928@gmail.com>
2023-09-13 02:16:56 +0900

    prettierの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import sample from "./assets/sample.png";
+import React from 'react';
+import sample from './assets/sample.png';
 
 export const App: React.FC = () => {
   return (
     <div>
-      <img src={sample} height={100} alt="" />
+      <img src={sample} height={100} alt='' />
       Hello World!!!!!!!
     </div>
   );
