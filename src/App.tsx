a1a725a89ea148a0b3e5d4145787f1b8d18665bf
KuharaNaoki <naoki.kuahra0928@gmail.com>
2023-10-03 01:13:43 +0900

    利用規約を反映

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TermsOfService } from './features/termsOfService/components/TermsOfService';
+
+function App() {
+  return (
+    <>
+      <TermsOfService />
+    </>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import sample from './assets/sample.png';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const Init = () => {
-  return (
-    <div>
-      <img src={sample} height={100} />
-      <div>Hello World!!!!!!!</div>
-    </div>
-  );
-};
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// createRoot を使用して初期化
-createRoot(document.getElementById('root')).render(<Init />);
+// // createRoot を使用して初期化
+// createRoot(document.getElementById('root')).render(<Init />);
